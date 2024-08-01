41f770c9adb35d7af5b382a34e32d434ea9aa7df
Nathan Lam <nathanl1605@gmail.com>
2024-08-01 15:09:22 +0100

    Completed Summary Section

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,7 +1,19 @@
+import '../Styles/summary.css';
+
 export default function ExpenseSummary(){
     return(
         <div className="summary-container">
-
+            <div className="expense-summary balance">
+                <h3 className="bal-name">Total Balance</h3>
+                <h3 className="bal-amount">€1000.<span className="cents">00</span></h3>
+            </div>
+            <div className="expense-summary total-exp">
+                <h3 className="exp-name">Total Expense</h3>
+                <h3 className="exp-amount">€1000.<span className="cents">00</span></h3>
+            </div>
+            <div className="expense-summary top-exp">
+            <h3 className="exp-name">Top Expenses</h3>
+            </div>
         </div>
     );
 }
