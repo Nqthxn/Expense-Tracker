c0735979d569dfd37bdd51e1f0604b45a7f1b338
Nathan Lam <nathanl1605@gmail.com>
2024-08-01 14:40:54 +0100

    Completed Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import '../Styles/header.css';
+import Profile from '../Icons/profile.png';
 
 export default function Header(){
     return(
         <header className="header-container">   
-            <span className="test">Hello</span>
+            <div className="profile-container">
+                <img src={Profile} className="profile"/>
+            </div>
+            <h1 className="header">Welcome back, Nathan!</h1>
         </header>
     )
 }
