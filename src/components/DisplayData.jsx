bb89c908c6fb1a0132ce43f16176d7fc888a985d
Nathan Lam <nathanl1605@gmail.com>
2024-08-06 15:06:57 +0100

    Storing user inputs in an array and displaying using .map()

--- src/components/DisplayData.jsx
+++ src/components/DisplayData.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import '../Styles/data.css';
 
-export default function DisplayData({data}){
-    React.useEffect(() => {
-        console.log("It worked");
-    }, [data]);
-
-    return(
-        <div>
-            {data ? (data.type) : (<p>No data available.</p>)}
+export default function DisplayData({ data }) {
+    return (
+        <div className="data-container">
+            <div className="expense-header">
+                <h3>Type</h3>
+                <h3>Description</h3>
+                <h3>Amount</h3>
+            </div>
+            <div className="expense-info">
+                {data.map((expense, index) => (
+                    <div className="expenses" key={index}>
+                        <p>{expense.type}</p>
+                        <p className="description">{expense.description}</p>
+                        <p>&#8364;{expense.amount}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -7,21 +7,21 @@ import DisplayData from './DisplayData.jsx';
 export default function Expenses() {
     const [modal, setModal] = React.useState(false);
     const [data, setData] = React.useState(null);
+    const [expenses, setExpenses] = React.useState([]);
 
     const toggleModal = (data) => {
         setModal(!modal);
     }
-    const handleData = (newData) => {
-        setData(newData);
+
+    const addExpense = (newExpense) => {
+        setExpenses(prevExpense => [...prevExpense, newExpense]);
+        setModal(false);
     }
-    
     return (
         <div className="expenses-container">
             {modal && (
-                <Modal onClick = {(newData) => {
-                    handleData(newData);
-                    toggleModal();
-                }}
+                <Modal 
+                    onClick = {addExpense}
                 />
             )}
             <div className="display-exp-container">
@@ -29,7 +29,7 @@ export default function Expenses() {
                     <h1 className="exp-title">Recent Expenses</h1>
                 </div>
                 <div className="expenses">
-                    {data && <DisplayData data={data}/>}
+                    {expenses.length > 0  ? (<DisplayData data={expenses}/>) : (<p className="no-expenses">No Expenses Recorded</p>)}
                 </div>
             </div>
             <div className="btn-container">
